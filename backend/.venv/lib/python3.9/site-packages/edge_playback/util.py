d11e261be942c6fa1da6fe8d090a26e3c0a16296
zhangzhang88 <68057668+zhangzhang88@users.noreply.github.com>
2025-11-16 16:14:14 +0800

    Remove .venv directory from repo

--- backend/.venv/lib/python3.9/site-packages/edge_playback/util.py
+++
@@ -1,8 +0,0 @@
-"""Utility functions for edge-playback"""
-
-import sys
-
-
-def pr_err(msg: str) -> None:
-    """Print to stderr."""
-    print(msg, file=sys.stderr)
--- backend/.venv/lib/python3.9/site-packages/edge_tts/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Main entrypoint for the edge-tts package."""
-
-from .util import main
-
-if __name__ == "__main__":
-    main()
